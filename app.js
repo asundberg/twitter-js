637c5cdd780ebb460e92ccc73182c0c59c54e2fb
Claire Dranginis <cdranginis@gmail.com>
2016-06-15 16:03:37 -0400

    added routes module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var swig = require('swig');
+var routes = require('./routes/');
 
 app.engine("html", swig.renderFile);
 app.set("view engine", "html");
@@ -8,26 +9,7 @@ app.set("views", __dirname + "/views");
 
 swig.setDefaults({ cache: false });
 
-app.use(function(req, res, next) {
-  console.log(req.method);
-  console.log(req.path);
-  next();
-});
-
-app.use('/special/', function(req, res, next) {
-  console.log('you reached this special area');
-  next();
-});
-
-app.get('/', function(req, res, next) {
-  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get('/news', function(req, res, next) {
-  res.send("This is the news page!");
-});
-
+app.use('/', routes);
 
 
 app.listen(3000, function() {
