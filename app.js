65c81b40798c0192d8194b452bba3221ed650400
Claire Dranginis <cdranginis@gmail.com>
2016-06-15 14:41:32 -0400

    added npm start to package json

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function() {
-  console.log("server listening");
+app.use(function(req, res, next) {
+  console.log(req.method);
+  console.log(req.path);
+  console.log(res.status());
+  next();
+});
+
+app.use('/special/', function(req, res, next) {
+  console.log('you reached this special area');
+  next();
 });
 
 app.get('/', function(req, res, next) {
   res.send("Welcome!");
+});
+
+app.get('/news', function(req, res, next) {
+  res.send("This is the news page!");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("server listening");
 });
